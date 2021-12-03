ea59c56214b82c268b4a9e4f0e5207e7048d4946
Komefumi <komefumi@protonmail.ch>
2021-12-03 23:25:15 +0530

    Add helper to read file without mentioning extension, where there is no ambiguity

--- File/index.rb
+++ File/index.rb
@@ -2,11 +2,20 @@
 
 module FileHelper
   # extend T::Sig
-  
   # sig {params(filename: String).returns(File)}
   def read_file(filename)
     return File.open(filename)
   end
+
+  def read_it(filename)
+    files = Dir.glob(filename) + Dir.glob("#{filename}.*")
+    if files.length == 1
+      return self.read_file(files[0])
+    else
+      raise "Ambiguity - More than one file matches"
+      puts files
+    end
+  end
   
-  module_function :read_file
+  module_function :read_file, :read_it
 end
