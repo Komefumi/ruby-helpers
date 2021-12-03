2dcb82636fb0fb739946c117c211d437e5b4345e
Komefumi <komefumi@protonmail.ch>
2021-12-03 23:33:23 +0530

    1. use read_it in read_file (making the latter an alias for the former) 2. make all_lines helper to pull in all lines from a file using read_it

--- File/index.rb
+++ File/index.rb
@@ -3,19 +3,23 @@
 module FileHelper
   # extend T::Sig
   # sig {params(filename: String).returns(File)}
-  def read_file(filename)
-    return File.open(filename)
-  end
-
   def read_it(filename)
     files = Dir.glob(filename) + Dir.glob("#{filename}.*")
     if files.length == 1
-      return self.read_file(files[0])
+      return File.open(files[0])
     else
       raise "Ambiguity - More than one file matches"
       puts files
     end
   end
-  
+
+  def read_file(filename)
+    return self.read_it(filename)
+  end
+
+  def all_lines(filename)
+    return self.read_it(filename).readlines
+  end
+
   module_function :read_file, :read_it
 end
